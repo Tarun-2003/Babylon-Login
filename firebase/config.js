56e259c8aa5ae40262d52a8d4aab083e93de35f5
Tarun-2003 <113117993+Tarun-2003@users.noreply.github.com>
2025-08-08 09:43:03 +0100

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: "1:449569154927:web:23ea8e8dad62af73912a9a"
 };
 
-// ✅ Prevent duplicate app initialization
+
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 const auth = getAuth(app);
